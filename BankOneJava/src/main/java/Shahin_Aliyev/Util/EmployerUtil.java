e90cc6fb0eca12bb276533b6d6b708225e1dddea
Shahin <sahin.aliyev979@gmail.com>
2019-05-13 00:31:55 +0200

    Update

--- BankOneJava/src/main/java/Shahin_Aliyev/Util/EmployerUtil.java
+++ BankOneJava/src/main/java/Shahin_Aliyev/Util/EmployerUtil.java
@@ -2,15 +2,13 @@ package Shahin_Aliyev.Util;
 
 import Shahin_Aliyev.Dao.impl.AccountImplDao;
 import Shahin_Aliyev.Dao.impl.IbanImplDao;
-import Shahin_Aliyev.Dao.impl.PercentageImplDao;
 import Shahin_Aliyev.beans.Account;
 import Shahin_Aliyev.beans.Customer;
-import Shahin_Aliyev.beans.IbanClass;
 import Shahin_Aliyev.beans.TypeOfAccount;
 
 public class EmployerUtil {
     public static boolean AccecptNewAccount(Account account) {
-     if(CheckAccount(account)!=null)
+     if(CheckAccount(account))
      {
          return true;
      }
@@ -21,84 +19,46 @@ public class EmployerUtil {
      }
 
     }
-    public static Account CheckAccount (Account account) {
+    public static boolean CheckAccount (Account account) {
 
            if(account.getCustomerAccount()==null || account.getTypeOfAccount()==null
             || account.getNumberOfAccount().trim().isEmpty())
            {
 
-               return  null;
+               return  false;
            }
            else {
-               return checkAccountBType(account);
+               return CheckInternationalAccount(account);
            }
 
 
     }
-    public static Account checkAccountBType(Account account)
-    {
-        if(account.getTypeOfAccount()==TypeOfAccount.Regular)
-        {
-            return account;
-        }
-        else
-        {
-            return checkSavingAndInternatinolAccount(account);
-        }
-    }
-    public static Account checkSavingAndInternatinolAccount(Account account)
-    {
+    public  static boolean CheckInternationalAccount(Account account) {
+        IbanImplDao ibanImplDao=new IbanImplDao();
         if(account.getTypeOfAccount()==TypeOfAccount.International)
         {
-            return CheckInternationalAccount(account);
-        }
-        else
-        {
-            return CheckSavingAccount(account);
-        }
-    }
-    public  static Account CheckSavingAccount(Account account) {
-        PercentageImplDao ibanImplDao=new PercentageImplDao();
-
-            if(ibanImplDao.getInterestByAccount(account).getPercentage()==0.01) {
-                return account;
-            }
-            else
-            {
-                return null;
-            }
-
-
-
-
-    }
-    public  static Account CheckInternationalAccount(Account account) {
-        IbanImplDao ibanImplDao=new IbanImplDao();
-
             if(ibanImplDao.getIbanByAccount(account)!=null) {
-                    return CheckIbanNumber(account);
+                    return CheckIbanNumber(ibanImplDao.getIbanByAccount(account).getIBAN());
                 }
             else
                 {
-                return null;
+                return false;
                 }
 
         }
 
+            return true;
 
 
-
-
-    public static Account CheckIbanNumber(Account account){
-        IbanImplDao ibanImplDao=new IbanImplDao();
-        String iBan=ibanImplDao.getIbanByAccount(account).getIBAN();
+    }
+    public static boolean CheckIbanNumber(String iBan){
       if(iBan.trim().isEmpty() || iBan.length()<10)
       {
-          return  null;
+          return  false;
       }
       else
       {
-          return account;
+          return true;
       }
 
     }
--- BankOneJava/src/test/java/Shahin_Aliyev/BankUtilTest.java
+++ BankOneJava/src/test/java/Shahin_Aliyev/BankUtilTest.java
@@ -2,8 +2,7 @@ package Shahin_Aliyev;
 
 import Shahin_Aliyev.Util.BankUtil;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+
 
 
 import static org.junit.Assert.*;
@@ -12,12 +11,10 @@ public class BankUtilTest {
 
     @Test
     public void createCustomer() {
-
     }
 
     @Test
     public void genarateCustomerNumber() {
-
         BankUtil bankUtil=new BankUtil();
         String result=bankUtil.genarateCustomerNumber();
         assertEquals(result,"00000");
