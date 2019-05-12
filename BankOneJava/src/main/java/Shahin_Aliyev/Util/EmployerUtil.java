5faac56dac52250dd352ca3f11e1480018056269
Shahin <sahin.aliyev979@gmail.com>
2019-05-13 00:33:32 +0200

    update

--- BankOneJava/src/main/java/Shahin_Aliyev/Util/EmployerUtil.java
+++ BankOneJava/src/main/java/Shahin_Aliyev/Util/EmployerUtil.java
@@ -2,64 +2,104 @@ package Shahin_Aliyev.Util;
 
 import Shahin_Aliyev.Dao.impl.AccountImplDao;
 import Shahin_Aliyev.Dao.impl.IbanImplDao;
+import Shahin_Aliyev.Dao.impl.PercentageImplDao;
 import Shahin_Aliyev.beans.Account;
 import Shahin_Aliyev.beans.Customer;
+import Shahin_Aliyev.beans.IbanClass;
 import Shahin_Aliyev.beans.TypeOfAccount;
 
 public class EmployerUtil {
     public static boolean AccecptNewAccount(Account account) {
-     if(CheckAccount(account))
-     {
-         return true;
-     }
-     else
-     {
-         return false;
-
-     }
-
-    }
-    public static boolean CheckAccount (Account account) {
-
-           if(account.getCustomerAccount()==null || account.getTypeOfAccount()==null
-            || account.getNumberOfAccount().trim().isEmpty())
-           {
-
-               return  false;
-           }
-           else {
-               return CheckInternationalAccount(account);
-           }
-
-
-    }
-    public  static boolean CheckInternationalAccount(Account account) {
-        IbanImplDao ibanImplDao=new IbanImplDao();
-        if(account.getTypeOfAccount()==TypeOfAccount.International)
+        if(CheckAccount(account)!=null)
         {
-            if(ibanImplDao.getIbanByAccount(account)!=null) {
-                    return CheckIbanNumber(ibanImplDao.getIbanByAccount(account).getIBAN());
-                }
-            else
-                {
-                return false;
-                }
+            return true;
+        }
+        else
+        {
+            return false;
 
         }
 
-            return true;
+    }
+    public static Account CheckAccount (Account account) {
+
+        if(account.getCustomerAccount()==null || account.getTypeOfAccount()==null
+                || account.getNumberOfAccount().trim().isEmpty())
+        {
+
+            return  null;
+        }
+        else {
+            return checkAccountBType(account);
+        }
 
 
     }
-    public static boolean CheckIbanNumber(String iBan){
-      if(iBan.trim().isEmpty() || iBan.length()<10)
-      {
-          return  false;
-      }
-      else
-      {
-          return true;
-      }
+    public static Account checkAccountBType(Account account)
+    {
+        if(account.getTypeOfAccount()==TypeOfAccount.Regular)
+        {
+            return account;
+        }
+        else
+        {
+            return checkSavingAndInternatinolAccount(account);
+        }
+    }
+    public static Account checkSavingAndInternatinolAccount(Account account)
+    {
+        if(account.getTypeOfAccount()==TypeOfAccount.International)
+        {
+            return CheckInternationalAccount(account);
+        }
+        else
+        {
+            return CheckSavingAccount(account);
+        }
+    }
+    public  static Account CheckSavingAccount(Account account) {
+        PercentageImplDao ibanImplDao=new PercentageImplDao();
+
+        if(ibanImplDao.getInterestByAccount(account).getPercentage()==0.01) {
+            return account;
+        }
+        else
+        {
+            return null;
+        }
+
+
+
+
+    }
+    public  static Account CheckInternationalAccount(Account account) {
+        IbanImplDao ibanImplDao=new IbanImplDao();
+
+        if(ibanImplDao.getIbanByAccount(account)!=null) {
+            return CheckIbanNumber(account);
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+
+
+
+
+    public static Account CheckIbanNumber(Account account){
+        IbanImplDao ibanImplDao=new IbanImplDao();
+        String iBan=ibanImplDao.getIbanByAccount(account).getIBAN();
+        if(iBan.trim().isEmpty() || iBan.length()<10)
+        {
+            return  null;
+        }
+        else
+        {
+            return account;
+        }
 
     }
     public static void LookPersonelDateOfCustomer(Customer customer) {
@@ -74,4 +114,4 @@ public class EmployerUtil {
 
         }
     }
-    }
+}
